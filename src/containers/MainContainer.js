840c7270022f6bdae40541294f87b7f0fd945961
kimberlybone <k_bone@uncg.edu>
2019-10-19 19:06:40 -0400

    css styling on login form

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import '../MainContainer.css';
 
 export default class MainContainer extends Component {
