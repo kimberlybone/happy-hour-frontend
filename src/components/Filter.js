25020ac7c0efb1a166f085ec214d54add39a99ef
kimberlybone <k_bone@uncg.edu>
2019-10-21 16:23:08 -0400

    Filter shows on page

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Filter extends Component {
-  handleCategories = ()
+
+  handleCategories = () => {
+    const {categories} = this.props
+    return categories.map(category => {
+      return (
+          < option key={category} value={category}>{category}< /option >
+      )
+    })
+  }
+
   render() {
     const {categories} = this.props
     console.log(categories)
-    return (<div></div>)
+    return (
+      <div>
+        Filter Drink By: < select onChange={}>
+          {this.handleCategories()}
+        < /select >
+      </div>
+    )
   }
 }
