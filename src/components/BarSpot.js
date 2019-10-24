cfab7a637a95af7a3ba4b61af90ee5201dd82036
mrwerner392 <mrwerner392@gmail.com>
2019-10-24 15:06:44 -0400

    drink pic with click

--- src/components/BarSpot.js
+++ src/components/BarSpot.js
@@ -17,22 +17,45 @@ export default class BarSpot extends Component {
   //   }
   // }
 
+  renderBarSpot = () => {
+    const { spotId, occupied, occupySpot, drinkUrl, consumeDrink } = this.props
 
+    const leftPos = `${parseInt(spotId.split('')[2])*10 + 5}%`
+
+    if (occupied) {
+      return (
+        <React.Fragment>
+          < img src={require('../Assets/brian-6.png')} alt='person' className={`person ${spotId}`} />
+        { drinkUrl ? < img className='drink-pic' style={{left: `${leftPos}`}} src={ drinkUrl } alt='drink' onClick={ consumeDrink } /> : null }
+        </React.Fragment>
+      )
+    } else {
+      return < img src={require('../Assets/bar-stool-2.png')}
+      alt='bar-stool'
+      onClick={ () => occupySpot(spotId) }
+      className={ `bar-stool ${spotId}` } />
+    }
+
+  }
 
   render() {
-    const { spotId, occupied, occupySpot } = this.props
+    const { props: { spotId }, renderBarSpot } = this
+
     return (
       < div className={ `bar-spot ${spotId}` } >
 
-      {
+      { renderBarSpot() }
+
+      {/*
         occupied ?
         < img src={require('../Assets/brian-6.png')} alt='person' className={`person ${spotId}`} />
+      <
         :
         < img src={require('../Assets/bar-stool-2.png')}
               alt='bar-stool'
               onClick={ () => occupySpot(spotId) }
               className={ `bar-stool ${spotId}` } />
-      }
+      */}
       < / div >
     )
   }
